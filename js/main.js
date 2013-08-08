a42c2ab5edefa904a231df521bd6c625c595279e
Marius <marius.wache@gmail.com>
2013-08-08 16:30:07 +0200

    bounds fix

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var dbg = false;
+var dbg = true;
 
 chrome.contextMenus.removeAll(function() {
     chrome.contextMenus.create({
@@ -46,6 +46,7 @@ chrome.alarms.onAlarm.addListener(function(alarm) {
 
 chrome.system.storage.getInfo(function(info) {
 	if(dbg) console.log(info);
+	var counter = 0;
 	for(disk in info) {
 		var dskname = 'HDD';
 		if(info[disk].name !== '') dskname = info[disk].name;
@@ -53,17 +54,15 @@ chrome.system.storage.getInfo(function(info) {
 						 '<header><i class="icon-hdd"></i></header>'+
 						 '<div><meter id="hdm" max="'+info[0].capacity+'" value="0" data-name="'+dskname+'" data-cap="0"></meter></div>'+
 						 '</div>');
-		var bounder = chrome.app.window.current().getBounds();
-		var newheight =	bounder.height + 40;
-		chrome.app.window.current().setBounds({'height': newheight});
-//		chrome.system.storage.addAvailableCapacityWatch(info[disk].id, function(info) {
-//			if(dbg) console.log(info);
-//			$('#mhd').attr('value', (info.capacity - info.availableCapacity));
-//		});
+		counter++;
 	}
+	var bounder = chrome.app.window.current().getBounds();
+	var newheight =	bounder.height + (counter * 40);
+	chrome.app.window.current().setBounds({'height': newheight});
 });
 
 chrome.system.storage.onAvailableCapacityChanged.addListener(function(info) {
+	if(dbg) console.log(info);
 	if(info.length) {
 		for(disk in info) {
 			$('#'+info[disk].id+' #hdm').attr('value',$('#'+info[disk].id+' #hdm').attr('max') - info[disk].availableCapacity);
