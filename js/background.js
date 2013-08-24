a4320c1e4fe6246e7c5602c1108821ce7d70db65
Mariu5 <marius.wache@gmail.com>
2013-08-25 01:12:03 +0200

    extension version

--- js/background.js
+++ js/background.js
@@ -1,28 +1,14 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-	chrome.app.window.create('html/widget.html', {
-		'id': 'widgetwin',
-		'frame': 'none',
-        'type': 'panel',
-		//'transparentBackground': true,
-		'resizable': false,
-		'singleton': true,
-		'maxWidth': 220/*,
-		'maxHeight': 160*/
-	}, function (appwindow) {
-		console.log('callback');
-		//appwindow.moveTo(0,0);
-		appwindow.setBounds({'height': 120});
-//		appwindow.onClosed.addListener(function() {
-//			console.log('close');
-//		});
+chrome.browserAction.onClicked.addListener(function() {
+	chrome.windows.create({
+		'url': '/html/widget.html',
+		'width': 220,
+		'height': 200,
+		'type': 'panel'
 	});
 });
 
-chrome.runtime.onSuspend.addListener(function() { 
-  // Do some simple clean-up tasks.
-});
 
-chrome.alarms.create('refresh', {
-    'when': Date.now(),
-    'periodInMinutes': 1
-});
+//chrome.alarms.create('refresh', {
+//    'when': Date.now(),
+//    'periodInMinutes': 1
+//});
